d302f74ad9a49c00c03f20df9a756ac21ea4f338
Krystal Richardson <krich24@Krystals-MBP.attlocal.net>
2023-04-10 16:38:12 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 // Assignment Code
+// variables being declared utilizing the query selector 
 var generateBtn = document.querySelector("#generate");
+var password = document.querySelector("#password");
 
-function generatePassword() {
-
-}
 // create an array of characters in order to generate random passwords when button is clicked  
-var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", '++'];
-var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", '++'];
-var characterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '+'];
-var numericArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var passwordText = document.querySelector("#password");
-
-var resultArray = [random];
-
-// password prompt generator question
-var randomPassword = prompt ('This will generate a random password 10 characters long')
-
-//conditions of the arrays 
-if (numeric) {
-  resultArray = resultArray.concat(numericArray)
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  passwordText.value = password;
-}
-
-
-
+var lowercaseArray = ['abcdefghijklmnopqrstuvwxyz'];
+var uppercaseArray = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+var characterArray = ['!@#$%^&*-_+'];
+var numericArray = ['0123456789'];
+var passwordCriteria = '';
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+
+// random password generator for results
+password.value === '';
+var randomPassword = '';
+
+// users must decide on their qualifications for the random password to be generated
+  var lowercase = confirm("Requires lowercase letters?");
+  var uppercase = confirm("Requires uppercase letters?");
+  var character = confirm("Requires special character?");
+  var numeric = confirm("Requires a number?");
+  var passwordLength = prompt("Please choose a number between 8 and 16 to generate your password");
+
+// prompt verification for password length
+if (passwordLength < 8 || passwordLength > 16) {
+  alert('Your password must be between 8 and 16 characters long, try again!')
+  var passwordLength = prompt("Please choose a number between 8 and 16 to generate your password");
+}
+// user must meet one of the following criteria in order to generate password
+else if (uppercase === false && uppercase === false && numeric === false && character === false) {
+  alert('Click ok to generate password following password criteria')
+  var lowercase = confirm("Requires lowercase letters?");
+  var uppercase = confirm("Requires uppercase letters?");
+  var character = confirm("Requires special character?");
+  var numeric = confirm("Requires a number?");
+}
+if (lowercase) {
+  passwordCriteria += lowercaseArray;
+}
+if (uppercase) {
+  passwordCriteria += uppercaseArray;
+}
+if (character) {
+  passwordCriteria += characterArray;
+}
+if (numeric) {
+  passwordCriteria += numericArray;
+}
+// allows a random password to be generated
+for (var i = 0; i < passwordLength; i++) {
+  randomPassword += passwordCriteria.charAt(Math.floor(Math.random() * passwordCriteria.length));
+}
+password.value = randomPassword;
+}
